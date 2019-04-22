8108ce366e05f43bb157a12436c28af4234680ed
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-04-22 23:23:24 +0530

    bump

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Hashtagify
-Generates hashtags for instagram posts
+# Imgur-Upload
+Just use javascript, no need jQuery, php.
+
+## Usage
+If you want to use your own clientID , you can change it [here](https://github.com/carry0987/Imgur-Upload/blob/master/js/upload.js#L11) :
+```
+new Imgur({ 
+    clientid: '4409588f10776f7', //You can change this ClientID
+    callback: feedback 
+});
+```
+
+## Demo
+[Demo](https://carry0987.github.io/Imgur-Upload/)
+
+## Download
+[Link](https://github.com/carry0987/Imgur-Upload/releases/)
---
+++ js/imgur.js
@@ -0,0 +1,166 @@
+/* Imgur Upload Script */
+(function (root, factory) {
+    "use strict";
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.Imgur = factory();
+    }
+}(this, function () {
+    "use strict";
+    var Imgur = function (options) {
+        if (!this || !(this instanceof Imgur)) {
+            return new Imgur(options);
+        }
+
+        if (!options) {
+            options = {};
+        }
+
+        if (!options.clientid) {
+            throw 'Provide a valid Client Id here: https://api.imgur.com/';
+        }
+
+        this.clientid = options.clientid;
+        this.endpoint = 'https://api.imgur.com/3/image';
+        this.callback = options.callback || undefined;
+        this.dropzone = document.querySelectorAll('.dropzone');
+        this.info = document.querySelectorAll('.info');
+
+        this.run();
+    };
+
+    Imgur.prototype = {
+        createEls: function (name, props, text) {
+            var el = document.createElement(name), p;
+            for (p in props) {
+                if (props.hasOwnProperty(p)) {
+                    el[p] = props[p];
+                }
+            }
+            if (text) {
+                el.appendChild(document.createTextNode(text));
+            }
+            return el;
+        },
+        insertAfter: function (referenceNode, newNode) {
+            referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+        },
+        post: function (path, data, callback) {
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.open('POST', path, true);
+            xhttp.setRequestHeader('Authorization', 'Client-ID ' + this.clientid);
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    if (this.status >= 200 && this.status < 300) {
+                        var response = '';
+                        try {
+                            response = JSON.parse(this.responseText);
+                        } catch (err) {
+                            response = this.responseText;
+                        }
+                        callback.call(window, response);
+                    } else {
+                        throw new Error(this.status + " - " + this.statusText);
+                    }
+                }
+            };
+            xhttp.send(data);
+            xhttp = null;
+        },
+        createDragZone: function () {
+            var p1, p2, input;
+
+                p1 = this.createEls('p', {}, 'Drop Image File Here');
+                p2 = this.createEls('p', {}, 'Or click here to select image');
+            input = this.createEls('input', {type: 'file', className: 'input', accept: 'image/*'});
+
+            Array.prototype.forEach.call(this.info, function (zone) {
+                zone.appendChild(p1);
+                zone.appendChild(p2);
+            }.bind(this));
+            Array.prototype.forEach.call(this.dropzone, function (zone) {
+                zone.appendChild(input);
+                this.status(zone);
+                this.upload(zone);
+            }.bind(this));
+        },
+        loading: function () {
+            var div, table, img;
+
+            div = this.createEls('div', {className: 'loading-modal'});
+            table = this.createEls('table', {className: 'loading-table'});
+            img = this.createEls('img', {className: 'loading-image', src: './css/loading-spin.svg'});
+
+            div.appendChild(table);
+            table.appendChild(img);
+            document.body.appendChild(div);
+        },
+        status: function (el) {
+            var div = this.createEls('div', {className: 'status'});
+
+            this.insertAfter(el, div);
+        },
+        matchFiles: function (file, zone) {
+            var status = zone.nextSibling;
+
+            if (file.type.match(/image/) && file.type !== 'image/svg+xml') {
+                document.body.classList.add('loading');
+                status.classList.remove('bg-success', 'bg-danger');
+                status.innerHTML = '';
+
+                var fd = new FormData();
+                fd.append('image', file);
+
+                this.post(this.endpoint, fd, function (data) {
+                    document.body.classList.remove('loading');
+                    typeof this.callback === 'function' && this.callback.call(this, data);
+                }.bind(this));
+            } else {
+                status.classList.remove('bg-success');
+                status.classList.add('bg-danger');
+                status.innerHTML = 'Invalid archive';
+            }
+        },
+        upload: function (zone) {
+            var events = ['dragenter', 'dragleave', 'dragover', 'drop'],
+                file, target, i, len;
+
+            zone.addEventListener('change', function (e) {
+                if (e.target && e.target.nodeName === 'INPUT' && e.target.type === 'file') {
+                    target = e.target.files;
+
+                    for (i = 0, len = target.length; i < len; i += 1) {
+                        file = target[i];
+                        this.matchFiles(file, zone);
+                    }
+                }
+            }.bind(this), false);
+
+            events.map(function (event) {
+                zone.addEventListener(event, function (e) {
+                    if (e.target && e.target.nodeName === 'INPUT' && e.target.type === 'file') {
+                        if (event === 'dragleave' || event === 'drop') {
+                            e.target.parentNode.classList.remove('dropzone-dragging');
+                        } else {
+                            e.target.parentNode.classList.add('dropzone-dragging');
+                        }
+                    }
+                }, false);
+            });
+        },
+        run: function () {
+            var loadingModal = document.querySelector('.loading-modal');
+
+            if (!loadingModal) {
+                this.loading();
+            }
+            this.createDragZone();
+        }
+    };
+
+    return Imgur;
+}));
